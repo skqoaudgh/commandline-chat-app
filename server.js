5521f24fddb80993cd679e100ad4c78c0bd14b7b
Cada <myungho.dev@gmail.com>
2019-12-20 15:44:17 +0900

    Node.js Express 서버 추가

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const Chatkit = require('@pusher/chatkit-server');
+
+const app = express();
+
+const chatkit = new Chatkit.default({
+    instanceLocator: 'YOUR INSTANCE LOCATOR',
+    key: 'YOUR KEY'
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.post('/users', (req, res) => {
+    const { username } = req.body
+    chatkit
+    .createUser({
+        id: username,
+        name: username
+    })
+    .then(() => {
+        console.log(`User created: ${username}`);
+        res.sendStatus(201);
+    })
+    .catch(err => {
+        if(err.error === 'services/chatkit/user_already_exists') {
+            console.log(`User already exists: ${username}`);
+            res.sendStatus(200);
+        } 
+        else {
+            res.status(err.status).json(err);
+        }
+    });
+});
+
+app.post('/authenticate', (req, res) => {
+    const authData = chatkit.authenticate({userId: req.query.user_id});
+    res.status(authData.status).send(authData.body);
+})
+
+app.listen(3001, err => {
+    if(err) {
+        console.log(err)
+    } 
+    else {
+        console.log(`Running on port ${port}`);
+    }
+});
